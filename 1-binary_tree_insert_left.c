7b29ec57862317d8d4d55fdd87c17331ce871381
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2024-01-13 17:02:46 -0800

    betty styling

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,9 +11,9 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
 
-	new = malloc(sizeof(binary_tree_t));
 	if (parent == NULL)
 		return (NULL);
+	new = malloc(sizeof(binary_tree_t));
 	if (new == NULL)
 		return (NULL);
 	new->right = NULL;
