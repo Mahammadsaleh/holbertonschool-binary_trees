8d1056a6afce46adc1bd97046df189cf62039fb2
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2024-01-14 03:42:44 -0800

    9th task was added

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,11 +11,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t lHeight;
 	size_t rHeight;
 
-	if (tree == NULL)
-		return 0;
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+		return (0);
 
 	lHeight = binary_tree_height(tree->left);
 	rHeight = binary_tree_height(tree->right);
 
-	return (lHeight > rHeight ? lHeight : rHeight) + 1;
+	return ((lHeight > rHeight ? lHeight : rHeight) + 1);
 }
