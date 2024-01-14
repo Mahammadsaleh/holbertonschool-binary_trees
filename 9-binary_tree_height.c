7f818effe8556691aebe2369b885161910fa75d0
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2024-01-14 03:20:43 -0800

    9th task was added

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,8 +1,20 @@
-size_t binary_tree_height(const binary_tree_t *tree)
+#include "binary_trees.h"
+/**
+ * binary_tree_height - find binary tree height
+ *
+ * @node: binary tree
+ *
+ * Return: height of tree
+ */
+size_t binary_tree_height(const binary_tree_t *node)
 {
-	size_t depth = 0;
+	if (node == NULL)
+		return (-1);
+	int lDepth = binary_tree_height(node->left);
+	int rDepth = binary_tree_height(node->right);
 
-	if (tree == NULL)
-		return;
-
+	if (lDepth > rDepth)
+		return (lDepth + 1);
+	else
+		return (rDepth + 1);
 }
