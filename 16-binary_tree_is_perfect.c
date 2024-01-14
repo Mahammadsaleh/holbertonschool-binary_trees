28608b489e70c7ab58fb242101b64fc02f1705e0
Nihad Gurbanov <7404@holbertonstudents.com>
2024-01-14 07:50:26 -0800

    16-binary_tree_is_perfect.c fixed

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 
-size_t get_depth(const binary_tree_t *tree, size_t depth);
+size_t get_depth(const binary_tree_t *tree);
 int is_perfect(const binary_tree_t *tree, size_t level, size_t depth);
 
 /**
@@ -11,10 +11,12 @@ int is_perfect(const binary_tree_t *tree, size_t level, size_t depth);
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+	size_t depth = get_depth(tree);
+
 	if (!tree)
 		return (0);
 
-	return is_perfect(tree, 0, get_depth(tree, 0) - 1);
+	return (is_perfect(tree, 0, depth));
 }
 
 /**
@@ -27,15 +29,15 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
  */
 int is_perfect(const binary_tree_t *tree, size_t level, size_t depth)
 {
-	if (!tree)
+	if (!tree->left && !tree->right)
 		return (level == depth);
 
 	if (!tree->left || !tree->right)
 		return (0);
 
 	level += 1;
-	return is_perfect(tree->left, level, depth) &&
-		   is_perfect(tree->right, level, depth);
+	return (is_perfect(tree->left, level, depth) &&
+		   is_perfect(tree->right, level, depth));
 }
 
 /**
@@ -45,11 +47,16 @@ int is_perfect(const binary_tree_t *tree, size_t level, size_t depth)
  *
  * Return: Depth of the node, 0 if tree is NULL.
  */
-size_t get_depth(const binary_tree_t *tree, size_t depth)
+size_t get_depth(const binary_tree_t *tree)
 {
-	if (!tree)
-		return (depth);
+	binary_tree_t *node = (binary_tree_t *)tree;
+	size_t depth = 0;
 
-	return get_depth(tree->left, depth + 1);
+	while (node)
+	{
+		depth += 1;
+		node = node->left;
+	}
+	return (depth - 1);
 }
 
