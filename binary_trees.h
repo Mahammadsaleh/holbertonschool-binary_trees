772d4993cbdc44ebea449bb263c4295483f32c37
Nihad Gurbanov <7404@holbertonstudents.com>
2024-01-14 06:32:59 -0800

    14-binary_tree_balance.c

--- binary_trees.h
+++ binary_trees.h
@@ -42,6 +42,10 @@ size_t binary_tree_depth(const binary_tree_t *tree);
 size_t binary_tree_size(const binary_tree_t *tree);
 size_t binary_tree_leaves(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
+
+int get_tree_height(const binary_tree_t *tree);
+int max(int a, int b);
+
 int binary_tree_balance(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
